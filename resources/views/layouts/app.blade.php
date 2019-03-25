1f0d223137219a27f99581b533e6fbdc6def9301
Patrick <pcallahan3@mail.greenriver.edu>
2019-03-25 15:43:24 -0700

    Set up SASS to be able to compile css with Node.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Basic Website</title>
+        <link rel="stylesheet" href="/css/app.css" >
     </head>
     <body>
+      @include('inc.navbar')
       @yield('content')
 
       @include('inc.sidebar')
