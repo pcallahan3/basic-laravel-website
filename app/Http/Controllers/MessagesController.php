028a7a909ce911c77b9a4c8392db6e5dd26ab743
Patrick <pcallahan3@mail.greenriver.edu>
2019-03-25 17:18:08 -0700

    Integrated form valadation using Collevetive API and created an includes template for the valadation messages

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class MessagesController extends Controller
 {
     public function submit(Request $request){
-      return $request->input('name');
+      $this->validate($request, [
+        'name' => 'required',
+        'email' => 'required'
+
+      ]);
+
+      return "Success";
 
     }
 }
